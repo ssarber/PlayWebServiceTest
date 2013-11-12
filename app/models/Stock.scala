c2fd649f07ef9d54ffdb491bdbc55c714466524e
Stan Sarber <ssarber@sungevity.com>
2013-11-12 12:19:21 -0800

    Fix indentation

--- app/models/Stock.scala
+++ app/models/Stock.scala
@@ -3,22 +3,22 @@ package models
 case class Stock(symbol: String, price: Double)
 
 object Stock {
-	
- 	import play.api.libs.json._
- 	
- 	// play.api.libs.json.Format
- 	implicit object StockFormat extends Format[Stock] {
+    
+    import play.api.libs.json._
+    
+    // play.api.libs.json.Format
+    implicit object StockFormat extends Format[Stock] {
 
- 		def reads(json: JsValue): JsResult[Stock] = {
- 			val symbol = (json \ "symbol").as[String]
- 			val price = (json \ "price").as[Double]
- 			JsSuccess(Stock(symbol, price))
- 		}
- 		
- 		def writes(s: Stock): JsValue = {
- 		  val stockAsList = Seq("symbol" -> JsString(s.symbol),
- 				  				"price" -> JsNumber(s.price))
- 		  JsObject(stockAsList)
- 		} 		
- 	}	
+        def reads(json: JsValue): JsResult[Stock] = {
+            val symbol = (json \ "symbol").as[String]
+            val price = (json \ "price").as[Double]
+            JsSuccess(Stock(symbol, price))
+        }
+        
+        def writes(s: Stock): JsValue = {
+          val stockAsList = Seq("symbol" -> JsString(s.symbol),
+                                "price" -> JsNumber(s.price))
+          JsObject(stockAsList)
+        }       
+    }   
 }
